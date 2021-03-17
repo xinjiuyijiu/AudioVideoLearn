5301862a8c14a23813e6011527274ffec828e3dd
liuchao <450127106@qq.com>
2021-03-17 16:59:09 +0800

    处理多摄像头调用：逻辑摄像头和物理摄像头的区别

--- CacheCamera/app/src/main/java/com/tz/cachecamera/MainActivity.kt
+++ CacheCamera/app/src/main/java/com/tz/cachecamera/MainActivity.kt
@@ -5,11 +5,14 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.*
-import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
+import android.hardware.camera2.params.OutputConfiguration
+import android.hardware.camera2.params.SessionConfiguration
+import android.media.ImageReader
+import android.os.*
 import android.util.Log
 import android.view.Surface
 import android.view.SurfaceHolder
@@ -18,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,6 +45,9 @@ class MainActivity : AppCompatActivity() {
         cameraHandler1 = Handler(handlerThread1.looper)
         initSurface()
         checkPermission()
+        textureView.postDelayed({
+            openMultiCamera()
+        }, 2000)
     }
 
     private fun initSurface() {
@@ -50,9 +57,10 @@ class MainActivity : AppCompatActivity() {
                 width: Int,
                 height: Int
             ) {
+
                 preview0SurfaceTexture = surface
-                preview0SurfaceTexture?.setDefaultBufferSize(1080, 1920)
-                openCamera("0")
+                preview0SurfaceTexture?.setDefaultBufferSize(720, 1280)
+                //openCamera("0")
             }
 
             override fun onSurfaceTextureSizeChanged(
@@ -73,7 +81,7 @@ class MainActivity : AppCompatActivity() {
 
         surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceCreated(holder: SurfaceHolder) {
-                openCamera("2")
+                // openCamera("1")
             }
 
             override fun surfaceChanged(
@@ -88,6 +96,43 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        initTextureView(surfaceView0)
+        initTextureView(surfaceView1)
+        initTextureView(surfaceView2)
+        initTextureView(surfaceView3)
+
+    }
+
+    val surfaceList = mutableListOf<SurfaceTexture>()
+
+    private fun initTextureView(tx: TextureView) {
+        tx.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                surfaceList.add(surface)
+                surface.setDefaultBufferSize(720, 1280)
+                //openCamera("0")
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture) = false
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+
+            }
+
+        }
     }
 
     private fun checkPermission() {
@@ -113,11 +158,23 @@ class MainActivity : AppCompatActivity() {
             )
 
             Log.i(TAG, "initCamera: direction ${cc.get(CameraCharacteristics.LENS_FACING)}")
+            val rac = cc.get(CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES)
+
             sb.append(
                 "camera ID: $ca \n camera support level: ${cc.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL)}\n" +
-                        "direction: ${cc.get(CameraCharacteristics.LENS_FACING)}\n"
+                        "direction: ${if (cc.get(CameraCharacteristics.LENS_FACING) == 0) "front" else "back"}\n" +
+                        "capabilities:  ${rac.contentToString()}\n" +
+                        "physical camera: ${
+                            cc.physicalCameraIds.toTypedArray().contentToString()
+                        }\n" +
+                        "\n\n"
             )
 
+            // CameraCharacteristics.REQUEST_AVAILABLE_CAPABILITIES_LOGICAL_MULTI_CAMERA
+            Log.i(TAG, "initCamera: $rac")
+            rac?.forEach {
+                Log.i(TAG, "initCamera: rac is $it")
+            }
 
             val sas = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
             val sizes = sas?.getOutputSizes(SurfaceTexture::class.java)
@@ -137,6 +194,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // ir 如果是local field 会导致 buffer abandon的错误，因为会被回收
+    val ir = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1)
+
     @SuppressLint("MissingPermission")
     private fun openCamera(cameraId: String) {
         cameraManager.openCamera(
@@ -147,15 +207,46 @@ class MainActivity : AppCompatActivity() {
 
                     val captureRequestBuilder =
                         camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                    var surface: Surface? = null
+
+                    val outputs = mutableListOf<Surface?>()
+
+
+                    var surface0: Surface? = null
                     if (cameraId == "0") {
-                        surface = Surface(preview0SurfaceTexture)
+                        surface0 = Surface(preview0SurfaceTexture)
+                        captureRequestBuilder.addTarget(surface0!!)  // 将CaptureRequest的构建器与Surface对象绑定在一起
+                        outputs.add(surface0)
                     }
 
-                    if (cameraId == "2") {
-                        surface = surfaceView.holder.surface
+
+                    var surface1: Surface? = null
+                    if (cameraId == "1") {
+                        surface1 = surfaceView.holder.surface
+                        captureRequestBuilder.addTarget(surface1!!)  // 将CaptureRequest的构建器与Surface对象绑定在一起
+                        outputs.add(surface1)
+                        // 添加一个ImageReader
+                        ir.setOnImageAvailableListener({
+                            // 这里捕获的每一帧数据，即时处理
+                            val image = it.acquireNextImage()
+                            val buffer = image.planes[0].buffer
+                            val pixels = ByteArray(buffer.remaining())
+                            buffer.get(pixels)
+                            image.close()
+
+                            val bitmap = BitmapFactory.decodeByteArray(pixels, 0, pixels.size)
+                            runOnUiThread {
+                                if (bitmap != null) {
+                                    this@MainActivity.image.setImageBitmap(bitmap)
+                                }
+                            }
+
+                        }, cameraHandler0)
+
+                        captureRequestBuilder.addTarget(ir.surface!!)
+                        outputs.add(ir.surface)
                     }
-                    captureRequestBuilder.addTarget(surface!!)  // 将CaptureRequest的构建器与Surface对象绑定在一起
+
+
                     captureRequestBuilder.set(
                         CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH
@@ -189,12 +280,13 @@ class MainActivity : AppCompatActivity() {
                             }
 
                         }
-                    val outputs = mutableListOf(surface)
                     camera.createCaptureSession(
                         outputs,
                         sessionStateCallback,
                         if (cameraId == "0") cameraHandler0 else cameraHandler1
                     )
+
+
                 }
 
                 override fun onDisconnected(camera: CameraDevice) {
@@ -209,6 +301,87 @@ class MainActivity : AppCompatActivity() {
             },
             if (cameraId == "0") cameraHandler0 else cameraHandler1
         )
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun openMultiCamera() {
+        cameraManager.openCamera("0", object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+
+
+                 val suface0 = Surface(surfaceList[0])
+                 val suface1 = Surface(surfaceList[1])
+                 val suface2 = Surface(surfaceList[2])
+                 val suface3 = Surface(surfaceList[3])
+                val physicalConfig1 = OutputConfiguration(suface0)
+                physicalConfig1.setPhysicalCameraId("2")
+                val physicalConfig2 = OutputConfiguration(suface1)
+                physicalConfig2.setPhysicalCameraId("3")
+                val physicalConfig3 = OutputConfiguration(suface2)
+                physicalConfig3.setPhysicalCameraId("3")
+                val physicalConfig4 = OutputConfiguration(suface3)
+                physicalConfig4.setPhysicalCameraId("3")
+                val outputs = mutableListOf(
+                    physicalConfig1,
+                    physicalConfig2,
+                    physicalConfig3,
+                    physicalConfig4
+                )
+
+                val mPreViewBuidler =
+                    camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                mPreViewBuidler.addTarget(suface0)
+                mPreViewBuidler.addTarget(suface1)
+                mPreViewBuidler.addTarget(suface2)
+                mPreViewBuidler.addTarget(suface3)
+//                mPreViewBuidler.set(
+//                    CaptureRequest.CONTROL_AE_MODE,
+//                    CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH
+//                )      // 闪光灯
+//                mPreViewBuidler.set(
+//                    CaptureRequest.CONTROL_AF_MODE,
+//                    CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+//                ) // 自动对焦
+
+                val config = SessionConfiguration(SessionConfiguration.SESSION_REGULAR,
+                    outputs,
+                    AsyncTask.SERIAL_EXECUTOR,
+                    object : CameraCaptureSession.StateCallback() {
+                        override fun onConfigured(session: CameraCaptureSession) {
+
+                            val cp = object : CameraCaptureSession.CaptureCallback() {
+                                override fun onCaptureCompleted(
+                                    session: CameraCaptureSession,
+                                    request: CaptureRequest,
+                                    result: TotalCaptureResult
+                                ) {
+                                    super.onCaptureCompleted(session, request, result)
+                                    Log.i(TAG, "onCaptureCompleted: ")
+                                }
+                            }
+
+                            session.setRepeatingRequest(
+                                mPreViewBuidler.build(),
+                                null,
+                                cameraHandler0
+                            )
+                        }
+
+                        override fun onConfigureFailed(session: CameraCaptureSession) {
+
+                        }
+                    })
+                camera.createCaptureSession(config)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+            }
+
+        }, cameraHandler0)
     }
 
     private fun isHardwareLevelSupported(c: CameraCharacteristics, requiredLevel: Int): Boolean {
